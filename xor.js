3adcbe13fdcfe7ce6b5519b1a97b058733f49f3b
vjefri <vjefri@gmail.com>
2015-10-18 12:58:20 -0700

    update xor

--- xor.js
+++ xor.js
@@ -1,22 +1,51 @@
 // return the values they don't share
-function complement(A, B) {
-    return A.filter(function(elem) {
-        return B.indexOf(elem) == -1;
-    });
-}
+// function complement(A, B) {
+//     return A.filter(function(elem) {
+//         return B.indexOf(elem) == -1;
+//     });
+// }
 
-function unique(arr) {
-    return arr.filter(function(elem, pos) {
-        return arr.indexOf(elem) == pos;
-    });
-}
+// function unique(arr) {
+//     return arr.filter(function(elem, pos) {
+//         return arr.indexOf(elem) == pos;
+//     });
+// }
+
+// function xor() {
+//     var args = [].slice.call(arguments);
+
+//     return args.map(unique).reduce(function(acc, val) {
+//      // get the unique values from one array, then from the next and join them. 
+//      return complement(acc, val).concat(complement(val, acc));
+//     });
+// }
+
+// another way
 
 function xor() {
-    var args = [].slice.call(arguments);
 
-    return args.map(unique).reduce(function(acc, val) {
-    	// get the unique values from one array, then from the next and join them. 
-    	return complement(acc, val).concat(complement(val, acc));
+    var arrays = [].slice.call(arguments);
+
+    var unique = function(array) {
+        return array.filter(function(val, index) {
+            return array.indexOf(val) == index
+        });
+    };
+
+    if (arguments.length == 1) return unique(arguments[0]);
+
+    return arrays.reduce(function(prevArr, currArr) {
+        var left = prevArr.filter(function(val) {
+            if (currArr.indexOf(val) == -1) return val;
+        });
+
+        var right = currArr.filter(function(val) {
+            if (prevArr.indexOf(val) == -1) return val;
+        });
+
+        var result = unique(left.concat(right));
+
+        return result;
     });
 }
 
